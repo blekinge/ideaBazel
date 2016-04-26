a6784e823716a8dc64a8ce09b57a6aff171f3d9f
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2016-04-26 14:53:56 +0200

    http://www.jetbrains.org/intellij/sdk/docs/tutorials/custom_language_support/language_and_filetype.html

---
+++ src/dk/statsbiblioteket/bazel/SimpleLanguage.java
@@ -0,0 +1,11 @@
+package dk.statsbiblioteket.bazel;
+
+import com.intellij.lang.Language;
+
+public class SimpleLanguage extends Language {
+    public static final SimpleLanguage INSTANCE = new SimpleLanguage();
+
+    private SimpleLanguage() {
+        super("Simple");
+    }
+}
